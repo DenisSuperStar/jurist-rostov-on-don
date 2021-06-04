d7293437b82b7646022c9fbc64851c60b27939ae
Denis Shklyarskij <den.shklyarskij@bk.ru>
2021-06-04 18:13:34 +0300

    created new folder for slick slider scripts files

---
+++ public/slider/initSlider.js
@@ -0,0 +1,9 @@
+$('.single-slider').slick({
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: true,
+    dots: true,
+    infinite: true,
+    mobileFirst: true,
+    pauseOnDotsHover: true
+});
