31aee3a5cb2d950ac2e005c5eb7d4ed99a1a86f9
Denis Shklyarskij <den.shklyarskij@bk.ru>
2021-06-14 19:00:56 +0300

    added new middlewares

--- index.js
+++ index.js
@@ -20,14 +20,32 @@ app.engine('hbs', createHbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', './views');
 
+//  обработка запроса к каталогу со статическими файлами
 app.use(express.static(path.resolve() + '/public'));
 
+// обработка запроса по корневому адресу /
 app.get('/', pages.index);
 
-app.get('/about', pages.about);
+// обработка запроса по адресу /about
+app.use('/about', pages.about);
 
+// обработка запроса по адресу /services
 app.get('/services', pages.services);
 
+// обработка запроса по адресу /contacts
 app.get('/contacts', pages.contacts);
 
-app.get('/contactUs', pages.contactUs);
+// обработка запроса по адресу /404
+app.get('/404', pages.notFound);
+
+// обработка запроса к несуществующей странице
+app.use((req, res) => {
+    res.status(404).sendFile(__dirname + '/public/404.html');
+});
+
+// обработка серверных ошибок
+app.use((err, req, res) => {
+    console.log(err.statusCode);
+    console.log(err.statusMessage);
+    res.status(500).sendFile(__dirname + '/public/500.html');
+});
