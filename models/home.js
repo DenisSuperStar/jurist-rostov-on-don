d4c3b90331f510eb23dd718d26a94ffa67881fbb
Denis Shklyarskij <den.shklyarskij@bk.ru>
2021-06-17 14:11:45 +0300

    added class file for read and convert db json data

---
+++ models/home.js
@@ -0,0 +1,13 @@
+
+const fs = require('fs');
+const path = require('path');
+
+module.exports = class File {
+    constructor() {
+        this.dataFile = fs.readFileSync(path.resolve() + '/data/home.json', "utf-8");
+    }
+
+    convertFileData() {
+        return JSON.parse(this.dataFile);
+    }
+}
