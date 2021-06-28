4adb1b67bf4726d6834884b617c12a5cce31d9ba
Denis Shklyarskij <den.shklyarskij@bk.ru>
2021-06-28 14:30:45 +0300

    added users model file

--- models/user.js
+++ models/user.js
@@ -1,33 +1,18 @@
-const mongoose = require('mongoose');
-const { Schema } = require('mongoose');
+const users = [];
 
-const userSchema = new Schema({
-    id: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    userName: {
-        type: String,
-        required: true
-    },
-    userEmail: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    userPhone: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    userMessage: {
-        type: String,
-        required: true
+module.exports = class User {
+    constructor(userName, userEmail, userPhone, userMessage) {
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.userPhone = userPhone;
+        this.userMessage = userMessage;
     }
-}, 
-{
-    versionKey: false
-});
 
-module.exports = mongoose.model('User', userSchema);
+    save() {
+        users.push(this);
+    }
+
+    static getAll() {
+        return users;
+    }
+}
