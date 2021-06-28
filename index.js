84c4b768f4fed8f3399a5cd22b6061a5fdb63449
Denis Shklyarskij <den.shklyarskij@bk.ru>
2021-06-28 14:33:44 +0300

    added handlers for new url addresses

--- index.js
+++ index.js
@@ -7,28 +7,24 @@ const pages = require('./controllers/pageRouterController.js');
 const contact = require('./controllers/contactUsController.js');
 const process = require('./controllers/processUserController.js');
 const server = http.createServer(app);
-// создаем парсер для данных с помощью модуля body parser
+
+// подключаем модуль парсинга
 const bodyParser = require('body-parser');
+// создаем экземпляр парсера
 const urlEncodedParser = bodyParser.urlencoded({
     extended: false
 });
-const mongoose = require('mongoose');
-const port = process.env.PORT || 3000;
+
+// установка значения порта
+const port = 3000;
+
+server.listen(port, () => {
+    console.log(`Сервер запущен на порту ${port}`);
+});
 
 const createHbs = expHbs.create({
     defaultLayout: 'main',
     extname: 'hbs'
-});
-
-const dbHost = '27017';
-const dbName = 'newClientDb';
-
-mongoose.connect(`mongodb://localhost:${dbHost}/${dbName}`, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
-    if (err) throw err;
-    
-    server.listen(port, () => {
-        console.log(`Сервер запущен на порту ${port}`);
-    });
 });
 
 app.engine('hbs', createHbs.engine);
@@ -45,10 +41,11 @@ app.get('/', urlEncodedParser, pages.index);
 // обработка запроса отправки формы по корневому адресу /
 app.post('/', urlEncodedParser, contact.contactUs);
 
-// обработка запроса по адресу /about
-app.get('/about', pages.about);
+// обработка гет-запроса по адресу /about
+app.get('/about', urlEncodedParser, process.addPerson);
 
-app.post('/about', urlEncodedParser, process.addUserComment);
+// обработка пост-запроса по адресу /about
+app.post('/about', urlEncodedParser, process.createPerson);
 
 // обработка запроса по адресу /price
 app.get('/price', pages.price);
@@ -57,7 +54,7 @@ app.get('/price', pages.price);
 app.get('/contacts', pages.contacts);
 
 // обработка запроса по адресу /dashboard
-app.get('/dashboard', process.showUserComment);
+app.get('/dashboard', process.getPerson);
 
 // обработка запроса по адресу /404
 app.get('/404', pages.notFound);
