d0f326f5a09ad0314896fa49ebf3ee78cac4fcbe
Denis Shklyarskij <den.shklyarskij@bk.ru>
2021-07-09 18:23:12 +0300

    added script of working of mobile menu

--- public/compScripts/header.js
+++ public/compScripts/header.js
@@ -1,16 +1,40 @@
 const header = $('.header');
 const menu = $('.index');
 let scrollPrev = 0;
+const btnHumb = $('#btnHumb');
+const dropdownHumb = $('#dropdownHumb');
+const dropdownList = $('#header__menu-humb');
+const dropdownItem = $('.header__item-humb');
 
-window.addEventListener('scroll', function() {
-    const scrolled = $(this).scrollTop();
+$(() => {
+    window.addEventListener('scroll', function() {
+        const scrolled = $(this).scrollTop();
+    
+        if ((scrolled > 100) && (scrolled > scrollPrev)) {
+            header.addClass('out');
+            menu.addClass('scroll');
+        } else {
+            header.removeClass('out');
+            menu.removeClass('scroll');
+        }
+        scrollPrev = scrolled; 
+    });
+    
+    btnHumb.on('click', () => {
+        if (dropdownHumb.hasClass('is-open')) {
+            dropdownHumb.removeClass('is-open');
+        } else {
+            dropdownHumb.addClass('is-open');
+        }
+    });
 
-    if ((scrolled > 100) && (scrolled > scrollPrev)) {
-        header.addClass('out');
-        menu.addClass('scroll');
-    } else {
-        header.removeClass('out');
-        menu.removeClass('scroll');
-    }
-    scrollPrev = scrolled; 
+    dropdownItem.on('click', e => {
+        $('.header__item-humb').each(function() {
+            $(this).removeClass('is-active');
+        });
+
+        if ($(e.target)) {
+            $(e.target).addClass('is-active');
+        }
+    });
 });
