339b7b52db5b4c70dde144ab6e1b23ca7cc57717
Denis Shklyarskij <den.shklyarskij@bk.ru>
2021-07-21 22:29:19 +0300

    created config database directory

---
+++ config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    port: 3000,
+    host: 'localhost:27017',
+    databaseName: 'orders',
+    databaseConf: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    }
+}
