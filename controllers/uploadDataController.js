49015a4c04a68f31c147b639592adb2312482a75
Denis Shklyarskij <den.shklyarskij@bk.ru>
2021-07-23 16:49:47 +0300

    cretead upload data controller

---
+++ controllers/uploadDataController.js
@@ -0,0 +1,37 @@
+module.exports.uploadAd = (req, res) => {
+    const body = JSON.parse(JSON.stringify(req.body));
+    const adName = body.adName;
+    const descName = body.descName;
+
+    if (!adName || !descName) return res.sendStatus(404);
+
+    const ad = new Ad({
+        adName: adName,
+        descName: descName
+    });
+
+    ad.save(err => {
+        if (err) res.sendStatus(500);
+        res.redirect('/');
+    });
+}
+
+module.exports.uploadItem = (req, res) => {
+    const body = JSON.parse(JSON.stringify(req.body));
+    const serviceName = body.serviceName;
+    const serviceDesc = body.serviceDesc;
+    const servicePrice = body.servicePrice;
+
+    if (!serviceName || !serviceDesc || !servicePrice) return res.sendStatus(404);
+    
+    const order = new Order({
+        serviceName: serviceName,
+        serviceDesc: serviceDesc,
+        servicePrice: servicePrice
+    });
+    
+    order.save(err => {
+        if (err) res.sendStatus(500);
+        res.redirect('/');
+    });
+}
