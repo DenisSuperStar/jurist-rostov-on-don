51389a4cc1575e96e5a11aaa0cd2ea2a07ef9133
Denis Shklyarskij <den.shklyarskij@bk.ru>
2021-07-31 12:50:10 +0300

    modified dashboard function

--- controllers/pageRouterController.js
+++ controllers/pageRouterController.js
@@ -52,6 +52,7 @@ module.exports.dashboard = (req, res) => {
         if (err) return res.sendStatus(500);
         res.render('dashboardView', {
             title: 'Панель расширенных возможностей',
+            persons: allPerson,
             isHidden: true,
             isInvisible: true
         });
