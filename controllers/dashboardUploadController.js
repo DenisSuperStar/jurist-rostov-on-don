fb7d4665259bd40eb43af4b35c3c2b7b16345e33
Denis Shklyarskij <den.shklyarskij@bk.ru>
2021-07-31 12:44:56 +0300

    renamed from upload data controller

--- controllers/dashboardUploadController.js
+++ controllers/dashboardUploadController.js
@@ -16,22 +16,31 @@ module.exports.uploadAd = (req, res) => {
     });
 }
 
-module.exports.uploadItem = (req, res) => {
+module.exports.uploadActivity = (req, res) => {
+
+}
+
+module.exports.uploadOrder = (req, res) => {
     const body = JSON.parse(JSON.stringify(req.body));
     const serviceName = body.serviceName;
     const serviceDesc = body.serviceDesc;
     const servicePrice = body.servicePrice;
 
     if (!serviceName || !serviceDesc || !servicePrice) return res.sendStatus(404);
-    
+
     const order = new Order({
         serviceName: serviceName,
         serviceDesc: serviceDesc,
         servicePrice: servicePrice
     });
-    
+
     order.save(err => {
-        if (err) res.sendStatus(500);
+        if (err) re.sendStatus(500);
+        
         res.redirect('/');
     });
+}
+
+module.exports.uploadService = (req, res) => {
+    
 }
