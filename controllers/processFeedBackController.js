a08447fb775595c26ba603f2a7e4ae961243a248
Denis Shklyarskij <den.shklyarskij@bk.ru>
2021-07-31 12:48:30 +0300

    renamed from process user controller.js

---
+++ controllers/processFeedBackController.js
@@ -0,0 +1,30 @@
+const Person = require('../models/person.js');
+
+module.exports.addPerson = (req, res) => {
+    res.render('aboutView', {
+        title: 'О компании "Юрист-на-Дону", Ростова-на-Дону',
+        isAbout: true
+    });
+}
+
+module.exports.createPerson = (req, res) => {
+    const body = JSON.parse(JSON.stringify(req.body));
+    const uName = body.userName;
+    const uEmail  = body.userEmail;
+    const uPhone = body.userPhone;
+    const uMessage = body.userMessage;
+
+    if (!uName || !uEmail || !uPhone || !uMessage) return res.sendStatus(400);
+
+    const person = new Person({
+        userName: uName,
+        userEmail: uEmail,
+        userPhone: uPhone,
+        userMessage: uMessage
+    });
+
+    person.save(err => {
+        if (err) return res.sendStatus(500);
+        res.redirect('/about');
+    });
+}
